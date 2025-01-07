7f5a0fda20eecfc13abb58b2c9dc9742d08449e7
Niilo Elias Räisänen <s4rani02@students.osao.fi>
2025-01-07 11:43:11 +0200

    pikku hommia

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,29 @@
-let header = document.querySelector('.header');
-
-header.classList.toggle('sticky', window.scrollY > 100);
+const colors = {
+    fiesta: "#DD4132",
+    jesterRed: "#9E1030",
+    turmeric: "#FE840E",
+    livingCoral: "#FF6F61",
+    pinkPeacock: "#C62168",
+    pepperStem: "#809440",
+    aspenGold: "#FFD662",
+    princessBlue: "#005390",
+    toffee: "#755139",
+    mangoMojito: "#D69C2F",
+    terrariumMoss: "#616247",
+    sweetLilac: "#E8B5CE",
+    soybean: "#D2C29D",
+    eclipse: "#343148",
+    sweetCorn: "#F0EAD6",
+    brownGranite: "#615550"
+  };
+  
+  // Vaihtaa ensimmäisen laatikon taustavärin
+  function varivaihto1(colorName) {
+    document.querySelector(".boxi img").style.backgroundColor = colors[colorName];
+  }
+  
+  // Vaihtaa toisen laatikon taustavärin
+  function varivaihto2(colorName) {
+    document.querySelector(".boxi2 img").style.backgroundColor = colors[colorName];
+  }
+  

5f15a00f97217c346214d5a5a2352b691611e674
Niilo Elias Räisänen <s4rani02@students.osao.fi>
2025-01-29 11:09:22 +0200

    ringring

--- Script/script.js
+++ Script/script.js
@@ -26,4 +26,33 @@ const colors = {
   function varivaihto2(colorName) {
     document.querySelector(".boxi2 img").style.backgroundColor = colors[colorName];
   }
+
+  // gallerian heiluseffecti
+
+  "use strict";
+(function () {
+	window.onload = () => {
+		const obj = document.querySelector("#gallery");
+		const time = 10000;
+		function animStart() {
+			if (obj.classList.contains("active") == false) {
+				obj.classList.add("active");
+				setTimeout(() => {
+					animEnd();
+				}, time);
+			}
+		}
+		function animEnd() {
+			obj.classList.remove("active");
+			obj.offsetWidth;
+		}
+		document.addEventListener("scroll", function () {
+			// scroll or scrollend
+			animStart();
+		});
+		window.addEventListener("resize", animStart);
+		animStart();
+	};
+})();
+
   
